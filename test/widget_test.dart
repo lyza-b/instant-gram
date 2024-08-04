5cfb587a7d540054037e2770ff3473b71206def3
Elizabeth Ajileye <ajioluwaseun1@gmail.com>
2024-08-04 06:09:48 +0100

    Uupdated Firebase configuration

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:lyza_insta_clone/main.dart';
 
-import 'package:my_ui_design_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
